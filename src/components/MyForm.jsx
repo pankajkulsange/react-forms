843c9480c14bf75faa7e19dfa4fcb068fba05649
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2023-10-07 12:57:34 +0530

    added handle function with simple consolelog

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 const MyForm = () => {
+  function handleFirstName(e) {
+    console.log("FirstName changed");
+  }
+  function handleLastName(e) {
+    console.log("LastName changed");
+  }
   return (
     <>
       <h3>MyForm</h3>
       <form>
         <label>First Name: </label>
-        <input type="text" placeholder="First Name" />
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={handleFirstName}
+        />
         <br />
         <label>Last Name: </label>
-        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Last Name" onChange={handleLastName} />
         <br />
         <button>Submit</button>
       </form>
